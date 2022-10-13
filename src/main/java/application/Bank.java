2e82c267737ef145ea4604de7f025ccfbfdc75ca
Elildes F Santos - Note <elildes.fortaleza.969@ufrn.edu.br>
2022-10-13 17:45:04 -0300

    update

--- src/main/java/application/Bank.java
+++ src/main/java/application/Bank.java
@@ -14,21 +14,6 @@ public class Bank {
 	private HashMap<Integer, Float> contas = new HashMap<>();
 	private String nome;
 
-/*	
-	public void HashMap<Integer, Float> getContas() {
-		return contas;
-	}
-*/
-	
-	public void addContas(int numconta) {
-		contas.put(numconta, 0.0f);
-	}
-	
-/*	
-	public void addContas(HashMap<Integer, Float> contas) {
-		this.contas = contas;
-	}
-*/	
 	public String getNome() {
 		return nome;
 	}
@@ -37,23 +22,40 @@ public class Bank {
 		this.nome = nome;
 	}
 	
+	public void addContas(int numconta) {
+		contas.put(numconta, 0.0f);
+	}
+	
 	@Post(router = "depositar")
 	public void depositar(int numconta, float valor) {
 		float atual = contas.get(numconta);
 		contas.put(numconta, atual+valor);
 	}
 	
+	//----JSon------------
 	@Get(router = "saldo")
 	public float saldo(int numconta) {
 		return contas.get(numconta);
 	}
 	
-	@Get JSONObject saldo(JSONObject jsonObject) throws Throwable {
-		// modelo objeto JSon
+	@Get(router = "saldo")
+	public JSONObject saldo (JSONObject jsonObj) throws Throwable {
 		
-		return jsonObject;
+		//numconta = jsonObj.getInteger(key: xx) 
+		
+		return null;
 		
 	}
 	
 	
 }
+
+/* Dependências:
+ * 
+ * 1. JSon:
+ * Fonte: https://github.com/stleary/JSON-java (Click here if you just want the latest release jar file)
+ * Install: Project -> Properties -> Java Build Path -> Libraries -> Classpath - > Add External Jars ...
+ * 
+ */
+
+
