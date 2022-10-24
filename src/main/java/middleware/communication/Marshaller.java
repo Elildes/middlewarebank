f93d4cd62f0f37e815b302f78aee6431d98b0134
Elildes F Santos - Note <elildes.fortaleza.969@ufrn.edu.br>
2022-10-24 19:31:05 -0300

    update marshaller

--- src/main/java/middleware/communication/Marshaller.java
+++ src/main/java/middleware/communication/Marshaller.java
@@ -1,5 +1,13 @@
 package main.java.middleware.communication;
 
+/*
+ * Converte invocações remotas em fluxos de bytes.
+ * O MARSALLER fornece um mecanismo genérico que não é
+ * específico para nenhum tipo de objeto remoto específico.
+ * Os REQUESTOR, INVOKER, and REQUEST HANDLERS usam o
+ * MARSHALLER para recuperar as informações de invocação
+ * contidas no fluxo de bytes da mensagem.
+ */
 public class Marshaller {
 
 }
