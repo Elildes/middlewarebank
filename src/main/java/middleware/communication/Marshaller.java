9b5555b97f73b0c6cf9ac9a61ea21eb77b543357
João Vìtor Fonseca <joovitor177@gmail.com>
2022-10-24 17:45:06 -0300

    marshall

--- src/main/java/middleware/communication/Marshaller.java
+++ src/main/java/middleware/communication/Marshaller.java
@@ -1,5 +1,50 @@
 package main.java.middleware.communication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Marshaller {
+	
+	public void Marshall() {}
+	
+	
+	// return a string array list with [method, route and body]
+	public ArrayList<String> Unmarshall(BufferedReader in) throws IOException {
+		String method = "";
+		String route = "";
+		String body = "";
+		ArrayList<String> contents = new ArrayList<String>();
+		String line;
+		while ((line = in.readLine()) != null) {
+	        String[] div = line.split(" ");
+	        method = div[0];
+	        route = div[1];
+			break;
+		}
+		
+		String text;
+		while ((text = in.readLine()) != null) {
+		     if (text.isEmpty()) {
+	                break;
+	            }
+	    }
+		
+        // read body
+		body = in.readLine();
+	
+	    contents.add(method);
+	    contents.add(route);
+	    contents.add(body);
+	    
+	    
+	    return contents;
+
+
+		
+
+		
+		
+	}
 
 }
