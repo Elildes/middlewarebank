dd3f7c11e7a2c42994711b91d492061d7bdb3789
João Fonseca <joao.vitor.081@ufrn.edu.br>
2022-10-25 14:41:39 +0000

    Update Marshaller.java

--- src/main/java/middleware/communication/Marshaller.java
+++ src/main/java/middleware/communication/Marshaller.java
@@ -4,22 +4,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/*
- * Converte invocações remotas em fluxos de bytes.
- * O MARSALLER fornece um mecanismo genérico que não é
- * específico para nenhum tipo de objeto remoto específico.
- * Os REQUESTOR, INVOKER, and REQUEST HANDLERS usam o
- * MARSHALLER para recuperar as informações de invocação
- * contidas no fluxo de bytes da mensagem.
- */
-
 public class Marshaller {
 	
-	public void Marshall() {}
+	
+	// return a String given a array listh with elements [código HTTP,  mensagem HTTP, conteúdo]
+	public String marshall(ArrayList<String> resposta) throws IOException {
+		
+        StringBuilder httpResponse = new StringBuilder();
+        httpResponse.append("HTTP/1.1 ");
+        httpResponse.append(resposta.get(0));
+        httpResponse.append(" ");
+        httpResponse.append(resposta.get(1));
+        httpResponse.append("\r\nUser-Agent: Autumn\r\nContent-Type: application/json\r\nContent-Length:");
+        httpResponse.append(resposta.get(2).getBytes().length);
+        httpResponse.append("\r\n\r\n");
+        httpResponse.append(resposta.get(2));
+        
+       return httpResponse.toString();
+        
+        
+		//ArrayList<String> contents = new ArrayList<String>();
+		
+		
+	}
 	
 	
 	// return a string array list with [method, route and body]
-	public ArrayList<String> Unmarshall(BufferedReader in) throws IOException {
+	public ArrayList<String> unmarshall(BufferedReader in) throws IOException {
 		String method = "";
 		String route = "";
 		String body = "";
