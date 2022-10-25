ab061756d668f6d4d24feb03653396f02bc972c4
João Fonseca <joao.vitor.081@ufrn.edu.br>
2022-10-25 14:44:48 +0000

    Update Invoker.java

--- src/main/java/middleware/communication/Invoker.java
+++ src/main/java/middleware/communication/Invoker.java
@@ -1,5 +1,17 @@
 package main.java.middleware.communication;
 
+import java.util.ArrayList;
+
 public class Invoker {
+	
+	// essa função deve receber um ArrayList de [método, rota e body]
+	// e retornar um ArrayList de [code, message, conteudo]
+	// com o objeto invocado
+	public ArrayList<String> invokar(ArrayList<String> intern) {
+	
+		ArrayList<String> resposta = new ArrayList<String>();
+	
+		return intern;
+}
 
 }
