ea46abc2b99a69f5c01e0ba566c20952675e62fe
João Vìtor Fonseca <joovitor177@gmail.com>
2022-10-26 10:33:07 -0300

    finalizando post

--- src/main/java/middleware/communication/Finance.java
+++ src/main/java/middleware/communication/Finance.java
@@ -23,18 +23,20 @@ public class Finance {
 				// Verfica se a anotação é um Get, Post, Put ou Delete:
 				if (method.isAnnotationPresent(Get.class)) {
 					method.setAccessible(true);		//permiti que o método seja acessado e invocado depois
-					RemoteObject.addMethodGet("get" + clazz.getAnnotation(RequestMap.class).router() + method.getAnnotation(Get.class).router(), method);	//Salva o método no hashmap (get + class route + method route)
+					RemoteObject.addMethodGet("GET" + clazz.getAnnotation(RequestMap.class).router() + method.getAnnotation(Get.class).router(), method);	//Salva o método no hashmap (get + class route + method route)
 				}else if (method.isAnnotationPresent(Post.class)) {
 					method.setAccessible(true);		//permiti que o método seja acessado e invocado depois
-	                RemoteObject.addMethodPost("post" + clazz.getAnnotation(RequestMap.class).router() + method.getAnnotation(Post.class).router(), method);	//Salva o método no hashmap (post + class route + method route)            	
+	                RemoteObject.addMethodPost("POST" + clazz.getAnnotation(RequestMap.class).router() + method.getAnnotation(Post.class).router(), method);	//Salva o método no hashmap (post + class route + method route)            	
 				}else if (method.isAnnotationPresent(Put.class)) {
 					method.setAccessible(true);		//permiti que o método seja acessado e invocado depois
-	                RemoteObject.addMethodPut("put" + clazz.getAnnotation(RequestMap.class).router() + method.getAnnotation(Put.class).router(), method);	//Salva o método no hashmap (put + class route + method route)           	
+	                RemoteObject.addMethodPut("PUT" + clazz.getAnnotation(RequestMap.class).router() + method.getAnnotation(Put.class).router(), method);	//Salva o método no hashmap (put + class route + method route)           	
 				}else if (method.isAnnotationPresent(Delete.class)) {
 					method.setAccessible(true);		//permiti que o método seja acessado e invocado depois
-	                RemoteObject.addMethodDelete("delete" + clazz.getAnnotation(RequestMap.class).router() + method.getAnnotation(Delete.class).router(), method);	//Salva o método no hashmap (delete + class route + method route)
+	                RemoteObject.addMethodDelete("DELETE" + clazz.getAnnotation(RequestMap.class).router() + method.getAnnotation(Delete.class).router(), method);	//Salva o método no hashmap (delete + class route + method route)
 	            }
 			}
+			
+			RemoteObject.printConcurrentHashMap();
 			return "método adicionado com sucesso";	
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
