ab535edcc0ab99e187c564655f503a07b8b4817a
Elildes F Santos - Note <elildes.fortaleza.969@ufrn.edu.br>
2022-10-26 22:54:30 -0300

    update

--- src/main/java/application/BankMiddleware.java
+++ src/main/java/application/BankMiddleware.java
@@ -7,6 +7,7 @@ import main.java.middleware.annotations.Post;
 import main.java.middleware.annotations.Put;
 import main.java.middleware.annotations.Delete;
 import main.java.middleware.annotations.RequestMap;
+//import main.java.application.Bank;
 
 /*
  * Implentada usando as anotações implementados pelo middleware.
@@ -17,6 +18,7 @@ import main.java.middleware.annotations.RequestMap;
 @RequestMap(router = "/fin")
 public class BankMiddleware {
 
+/*
 	// Adicionar saldo à conta
 	@Post(router = "/add")
 	public JSONObject add(JSONObject jsonObject) throws Throwable {
@@ -34,12 +36,44 @@ public class BankMiddleware {
 		
 		return result;
 	}
+*/
+	
+	// Adicionar conta bancária
+	@Post(router = "/add")
+	public JSONObject add(JSONObject jsonObject) throws Throwable {
+
+		// pegar variável JSon: body = {"var1": "num_conta", "var2": "num_agencia"}
+		String var1 = jsonObject.getString("var1");
+		System.out.println("\nDEBUG: O valor do var1 = " + var1);
+
+		String var2 = jsonObject.getString("var2");
+		System.out.println("\nDEBUG: O valor de var2 = " + var2);
+
+		// instância da classe de negócio
+		BankDB dbBanco = BankDB.getInstance();
+
+		Bank conta = new Bank();
+		conta.setAgencia("1234");
+		conta.setNumero("12000");
+		dbBanco.addBank(conta);
+		
+		JSONObject result = new JSONObject();
+		result.put("var1", var1);
+		result.put("var2", var2);
+		
+		// Teste:
+		System.out.println("DEBUG: array_contas em fin/add:\n" + BankDB.getInstance().listarBancos());
+		String str = result.toString();
+		System.out.println("DEBUG: result em fin/add:\n" + str);
+		
+		return result;
+	}
+	
 	
 	// fazer depósito bancário
 	@Post(router = "/dep")
 	public JSONObject dep(JSONObject jsonObject) throws Throwable {
-		
-		
+			
 		return jsonObject;
 	}
 	
